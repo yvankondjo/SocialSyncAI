4d1f91382bb0f0a8b870dff1010dfa3d449195ec
yvankondjo <yvankondjo8@gmail.com>
2025-07-28 19:23:05 +0000

    auth

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  webpack: (config: any) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
